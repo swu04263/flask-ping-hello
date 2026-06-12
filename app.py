1a00ef0ddf626a15672e80af3d702b5af02fc154
swu04263 <swu04263@gmail.com>
2026-06-12 20:06:02 +0800

    Auto-merge PR #1
    Co-authored-by: swu04263 <swu04263@users.noreply.github.com>

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    return 'pong'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
